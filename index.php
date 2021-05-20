476e316adfdc37a070d67172dde5ff44c3f63af3
Lawrence Gorris <gorrislawrence.personal@gmail.com>
2021-05-20 10:00:30 +0200

    Solved exercises 1 and 2

--- index.php
+++ index.php
@@ -10,11 +10,17 @@ error_reporting(E_ALL);
 // sometimes, even your IDE can tell you what's wrong
 echo "Exercise 1 starts here:";
 
-function new_exercise() {
-    $x = 1;
+function new_exercise($x) {
     $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
     echo $block;
-
 }
-
 new_exercise(2);
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0]; // SOLUTION: MONDAY HAS INDEX 0 IN THE ARRAY $week
+
+echo $monday;
+
