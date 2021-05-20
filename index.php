45e95dab57ad29a15ba408787d53b53aae4d7af2
Lawrence Gorris <gorrislawrence.personal@gmail.com>
2021-05-20 11:50:13 +0200

    Added exercises 3 and 4

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
-ini_set('display_errors', (string)1);
-ini_set('display_startup_errors', (string)1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // === Exercise 1 ===
@@ -24,3 +24,11 @@ $monday = $week[0]; // SOLUTION: MONDAY HAS INDEX 0 IN THE ARRAY $week
 
 echo $monday;
 
+new_exercise(3);
+// === Exercise 3 ===
+// This should echo ` "Debugged !" `, fix it so that that is the literal text echoed
+$str = "Debugged ! Also very fun";
+echo substr($str, 0, 10);
+
+
+new_exercise(4);
