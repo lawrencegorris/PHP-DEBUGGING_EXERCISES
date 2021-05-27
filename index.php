1a2ccd2c5e9ac0c5a38849b7010bc5229106cf42
Lawrence Gorris <gorrislawrence.personal@gmail.com>
2021-05-27 09:58:08 +0200

    Added all exercises except nr. 10

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ function new_exercise($x) {
     $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
     echo $block;
 }
+
 new_exercise(2);
 // === Exercise 2 ===
 // Below we create a week array with all days of the week.
@@ -21,8 +22,8 @@ new_exercise(2);
 
 $week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 $monday = $week[0]; // SOLUTION: MONDAY HAS INDEX 0 IN THE ARRAY $week
-
 echo $monday;
+
 
 new_exercise(3);
 // === Exercise 3 ===
@@ -36,9 +37,92 @@ new_exercise(4);
 // Sometimes debugging code is just like looking up code and syntax...
 // The print_r($week) should give:  Array ( [0] => mon [1] => tues [2] => wednes [3] => thurs [4] => fri [5] => satur [6] => sun )
 // Look up whats going wrong with this code, and then fix it, with ONE CHARACTER!
-
 foreach($week as &$day) {
     $day = substr($day, 0, strlen($day)-3);
 }
 
 print_r($week);
+
+new_exercise(5);
+// === Exercise 5 ===
+// The array should be printing every letter of the alphabet (a-z) but instead it does that + aa-yz
+// Fix the code so the for loop only pushes a-z in the array
+$arr = [];
+for ($letter = 'a'; $letter !='aa'; $letter++) {
+    array_push($arr, $letter);
+}
+print_r($arr);
+
+
+new_exercise(6);
+// === Final exercise ===
+// The fixed code should echo the following at the bottom:
+// Here is the name: $name - $name2
+// $name variables are decided as seen in the code, fix all the bugs whilst keeping the functionality!
+$arr = [];
+
+function combineNames($str1 = "", $str2 = "")
+{
+    $params = [$str1, $str2];
+    foreach($params as &$param) {
+        if ($param == "") {
+            $param = randomHeroName();
+        }
+    }
+    return implode(" - ", $params);
+}
+
+
+function randomHeroName(): string
+{
+    $hero_firstnames = ["captain", "doctor", "iron", "Hank", "ant", "Wasp", "the", "Hawk", "Spider", "Black", "Carol"];
+    $hero_lastnames = ["America", "Strange", "man", "Pym", "girl", "hulk", "eye", "widow", "panther", "daredevil", "marvel"];
+    $heroes = [$hero_firstnames, $hero_lastnames];
+    return $heroes[rand(0,count($heroes)-1)][rand(0, 10)];
+}
+
+echo "Here is the name: " . combineNames();
+
+new_exercise(7);
+function copyright(int $year) {
+    return "&copy; $year BeCode";
+}
+//print the copyright
+print copyright(idate('Y'));
+
+new_exercise(8);
+function login(string $email, string $password) : string {
+    if($email == 'john@example.be' && $password == 'pocahontas') {
+        echo 'Welcome John Smith <br>';
+    }
+        return 'No access <br>';
+}
+
+//do not change anything below
+//should great the user with his full name (John Smith)
+echo login('john@example.be', 'pocahontas');
+//no access
+echo login('john@example.be', 'dfgidfgdfg');
+//no access
+echo login('wrong@example.be', 'wrong');
+//you can change things again!
+
+new_exercise(9);
+function isLinkValid(string $link) {
+    $unacceptables = array('https:','.doc','.pdf', '.jpg', '.jpeg', '.gif', '.bmp', '.png');
+
+    foreach ($unacceptables as $unacceptable) {
+        if (strpos($link, $unacceptable) !== false) {
+            return 'Unacceptable Found<br />';
+        }
+    }
+    return 'Acceptable<br />';
+}
+//invalid link
+echo isLinkValid('http://www.google.com/hack.pdf');
+//invalid link
+echo isLinkValid('https://google.com');
+//VALID link
+echo isLinkValid('http://google.com');
+//VALID link
+echo isLinkValid('http://google.com/test.txt');
